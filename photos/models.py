2282b739b7a009d637ef283e67614ba54c6a74e1
Marvin Chomba <marvin.chomba24@gmail.com>
2018-12-14 07:41:30 +0300

    Add the image,comment and profile model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Image(models.Model):
+    image_url = models.ImageField(upload_to = 'pics')
+    name = models.CharField(unique = True,max_length = 31)
+    caption = models.CharField(max_length = 50)
+    likes = models.ManyToManyField(User, related_name = "likes")
+    user = models.ForeignKey(User)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,related_name = "profile")
+    bio = models.CharField(max_length = 40)
+    pic = models.ImageField(upload_to = 'pics')
+
+class Comments(models.Model):
+    image = models.ForeignKey(Image, related_name = "comments")
+    user = models.ForeignKey(User, related_name = "comments")
