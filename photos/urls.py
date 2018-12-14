31a2ab238c5b6a87d3421f6ab8e54511c307aced
Marvin Chomba <marvin.chomba24@gmail.com>
2018-12-14 08:47:39 +0300

    Add the like functionality

--- photos/urls.py
+++ photos/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from django.conf.urls.static import static
-from djanfo.conf import settings
+from django.conf import settings
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    url(r"^$", views.index, name = 'index'),
+    url(r"^like/(\d+)/$", views.like, name = "like")
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Image,Follow,Comments,Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url = "accounts/login")
+def index(request):
+    images = Image.objects.all()
+    return render(request,"index.html", {"images":images})
+
+def like(request,id):
+    user = request.user
+    image = Image.objects.get(id = id)
+    if user in image.likes.all():
+        image.likes.remove(user)
+    else:
+        image.likes.add(user)
+    return redirect("index")
